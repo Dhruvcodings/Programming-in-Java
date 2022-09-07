ac85dcb5ad89094ecb7c2befd216ef51a8452aca
DHRUV GARG <dhruvgarg7003@gmail.com>
2022-09-07 21:55:47 +0530

    Add files via upload

---
+++ decimal_binary.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class prime_number {
+    public static void dectobin(int decnum){
+        int pow=0;
+        int binnum=0;
+        int mynum=decnum;
+        while (decnum>0){
+            int rem=decnum%2;
+            binnum=binnum+(rem* (int)Math.pow(10,pow));
+            pow++;
+            decnum=decnum/2;
+        }
+
+            System.out.println("binary of "+mynum+" = "+binnum);
+        }
+    
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int number=sc.nextInt();
+        dectobin(number);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
