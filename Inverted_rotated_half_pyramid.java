456221585dc0279506c709091b4e627a7cb65c5b
DHRUV GARG <dhruvgarg7003@gmail.com>
2022-09-18 17:08:52 +0530

    Create Inverted_rotated_half_pyramid.java

---
+++ Inverted_rotated_half_pyramid.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Inverted_rotated_half_pyramid {
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int column=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=row-i;j++){
+                System.out.print(" ");
+                }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+       
+        
+    }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
